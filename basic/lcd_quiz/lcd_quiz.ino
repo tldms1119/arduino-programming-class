1b968343ba46abce1cb52f6dc8904aa508351aa9
sieun <tldms1119@naver.com>
2018-12-06 11:31:13 +0900

    LCD quiz

---
+++ basic/lcd_quiz/lcd_quiz.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+boolean toggle = false;
+
+void setup() {
+  lcd.begin();
+  lcd.backlight();
+}
+
+void loop() {
+  lcd.clear();
+
+  if(toggle == true){
+    lcd.print(" (^o^)   (ToT) ");
+    toggle = false;
+  } else {
+    lcd.print(" (ToT)   (^o^) ");
+    toggle = true;
+  }
+  
+  delay(1000);
+}
